ef09ded862473736b75828ee981d8c1c4f14f834
lemarcelo <lemarcelo@outlook.com>
2022-06-24 01:18:07 -0300

    Inclusão de tratamento de erro e alterações no estilo

---
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -0,0 +1,22 @@
+import { first, tap ,delay} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http';
+
+import { Course } from './../model/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+private readonly API = '/assets/courses.json';
+constructor(private httpClient: HttpClient) {}
+
+list(){
+  return this.httpClient.get<Course[]>(this.API)
+  .pipe(
+    first(),
+    delay(5000),
+    tap(courses => console.log(courses))
+  );
+  }
+}
